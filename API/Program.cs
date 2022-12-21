b390d506ff8bde15c2b4dcf0980b67470196032e
Marcin <Marcin@DESKTOP-L62L7NR>
2022-12-21 23:36:37 +0100

    fix in connection string

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options => 
 {
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultsConnection"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
