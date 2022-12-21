8988ef0f2f6d9a5c7b1be36faa38f6692f63aa51
Marcin <Marcin@DESKTOP-L62L7NR>
2022-12-22 00:15:44 +0100

    dbcontext injected, ctor created

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
         
+
     }
 }
