778739f89292ec81a32032f2345d735b286628b6
Marcin <Marcin@DESKTOP-L62L7NR>
2022-12-25 12:12:41 +0100

    Add symmetric key

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration configuration)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+        }
+
         public string CreateToken(User user)
         {
-            
+
         }
     }
 }
