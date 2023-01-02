5542966c69fc439cd0aded401d3923cd472c1b23
Marcin <Marcin@DESKTOP-L62L7NR>
2023-01-02 21:06:14 +0100

    Add errors interceptor

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -3,16 +3,47 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse)=>{
+        if(error){
+          switch(error.status){
+            case 400:
+              if(error.error.errors){
+                const modalStateErros = [];
+                for(const key in error.error.errors){
+                  if(error.error.errors[key]){
+                    modalStateErros.push(error.error.errors[key])
+                  }
+                }
+
+                throw modalStateErros;
+              }
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500:
+              const navigationExtras: NavigationExtras = {state: { error: error.error}};
+              this.router.navigateByUrl('/server-error', navigationExtras);
+            default:
+              console.log('something unexpected went wrong');
+              break;
+          }
+        }
+        throw error;
+      })
+    )
+      
   }
 }
