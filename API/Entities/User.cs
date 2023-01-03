c4bf6eb7c4077c1c6d78e2f34f75e51486634632
Marcin <Marcin@DESKTOP-L62L7NR>
2023-01-03 18:34:47 +0100

    Add photo entity

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -16,5 +16,6 @@ namespace API.Entities
         public string Interests{ get; set; } 
         public string City{ get; set; } 
         public string Country{ get; set; } 
+        public List<Photo> Photos { get; set; } = new List<Photo>{};
     }
 }
