c3355bb3f396528bd7e7140500884b564b90865d
Marcin <Marcin@DESKTOP-L62L7NR>
2023-01-06 20:22:38 +0100

    Save seed users to db

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -14,6 +18,30 @@ namespace API.Data
             {
                 return;
             }
+
+            var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+
+            var users = JsonSerializer.Deserialize<List<User>>(userData);
+
+            SaveUsersPasswords(users, dataContext);
+
+            await dataContext.SaveChangesAsync();
         } 
+
+        public static void SaveUsersPasswords(List<User> users, DataContext dataContext)
+        {
+            foreach(var user in users)
+            {
+                using var hmac = new HMACSHA512();
+                user.UserName = user.UserName.ToLower();
+
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+                dataContext.Users.Add(user);
+            }
+
+        }
     }
 }
