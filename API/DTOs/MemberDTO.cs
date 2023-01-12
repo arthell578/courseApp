a829de2616ae62e6a5ce235873d6058c2f2d1366
Marcin <Marcin@DESKTOP-L62L7NR>
2023-01-12 23:10:38 +0100

    Remove unnecessary props initializations

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -11,14 +11,14 @@ namespace API.DTOs
         public string UserName { get; set; }
         public string Age{ get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; } = DateTime.UtcNow;
-        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; } 
         public string Gender{ get; set; } 
         public string Introduction{ get; set; } 
         public string LookingFor{ get; set; } 
         public string Interests{ get; set; } 
         public string City{ get; set; } 
         public string Country{ get; set; } 
-        public List<PhotoDTO> Photos { get; set; } = new List<PhotoDTO>{};
+        public List<PhotoDTO> Photos { get; set; }
     }
 }
