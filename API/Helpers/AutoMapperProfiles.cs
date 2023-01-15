160ce3293d124b898a74798b0be17feec8c2714c
Marcin <Marcin@DESKTOP-L62L7NR>
2023-01-15 22:19:52 +0100

    Create maps between user, photo into DTOs

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+using AutoMapper.Execution;
 
 namespace API.Helpers
 {
-    public class AutoMapperProfiles
+    public class AutoMapperProfiles : Profile
     {
-        
+        protected AutoMapperProfiles()
+        {
+            CreateMap<User, MemberDTO>();
+            CreateMap<Photo, PhotoDTO>();
+        }
     }
 }
